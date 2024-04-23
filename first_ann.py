bc83a814ab957c98901c5d0b9ec92fac3e6fc97f
WantaNathaen <nmwanta101@gmail.com>
2024-04-22 23:45:19 -0500

    a first attempt with ANN using the MNIST dataset

--- first_ann.py
+++ first_ann.py
@@ -5,51 +5,103 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 
 BATCH_SIZE = 128
+EPOCHS = 10
+LEARNING_RATE = 0.001
+
 
 class FeedForwardNet(nn.Module):
 
+    # class constructor
     def __init__(self):
         super().__init__()
         self.flatten = nn.Flatten()
         self.dense_layers = nn.Sequential(
-            nn.Linear(28*28, 256),
+            nn.Linear(28 * 28, 256),
             nn.ReLU(),
             nn.Linear(256, 10)
         )
         self.softmax = nn.Softmax(dim=1)
 
+    # method for moving forward through the layers
     def forward(self, input_data):
-        flattened_data = self.flatten(input_data)
-        logits = self.dense_layers(flattened_data)
+        x = self.flatten(input_data)
+        logits = self.dense_layers(x)
         predictions = self.softmax(logits)
         return predictions
 
 
+# downloading the pre-included MNIST dataset in PyTorch
 def download_mnist_datasets():
     train_data = datasets.MNIST(
         root="data",
-        download=True,
         train=True,
-        transform=ToTensor()
+        download=True,
+        transform=ToTensor(),
     )
     validation_data = datasets.MNIST(
         root="data",
-        download=True,
         train=False,
-        transform=ToTensor()
+        download=True,
+        transform=ToTensor(),
     )
     return train_data, validation_data
 
 
+def create_data_loader(train_data, batch_size):
+    train_dataloader = DataLoader(train_data, batch_size=batch_size)
+    return train_dataloader
+
+
+# running through the training process once
+def train_single_epoch(model, data_loader, loss_fn, optimiser, device):
+    for input, target in data_loader:
+        input, target = input.to(device), target.to(device)
+
+        # calculate loss
+        prediction = model(input)
+        loss = loss_fn(prediction, target)
+
+        # backpropagate error and update weights
+        optimiser.zero_grad()
+        loss.backward()
+        optimiser.step()
+
+    print(f"loss: {loss.item()}")
+
+
+# running through the training process epochs number of times
+def train(model, data_loader, loss_fn, optimiser, device, epochs):
+    for i in range(epochs):
+        print(f"Epoch {i+1}")
+        train_single_epoch(model, data_loader, loss_fn, optimiser, device)
+        print("---------------------------")
+    print("Finished training")
+
+# driving code
 if __name__ == "__main__":
+
+    # download data and create data loader
     train_data, _ = download_mnist_datasets()
-    print("MNIST dataset downloaded")
+    train_dataloader = create_data_loader(train_data, BATCH_SIZE)
 
-# 1- data loader
-    train_data_loader = DataLoader(train_data, batch_size=BATCH_SIZE)
+    # construct model and assign it to device (allows use of CUDA
+    # if available, otherwise, default to CPU)
+    if torch.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
+    print(f"Using {device}")
+    feed_forward_net = FeedForwardNet().to(device)
+    print(feed_forward_net)
 
-# 2- build model
+    # initialise loss (cost) function + optimiser
+    loss_fn = nn.CrossEntropyLoss()
+    optimiser = torch.optim.Adam(feed_forward_net.parameters(),
+                                 lr=LEARNING_RATE)
 
-# 3- train
+    # train model
+    train(feed_forward_net, train_dataloader, loss_fn, optimiser, device, EPOCHS)
 
-# 4- save trained model
+    # save model
+    torch.save(feed_forward_net.state_dict(), "feedforwardnet.pth")
+    print("Trained feed forward net saved at feedforwardnet.pth")
